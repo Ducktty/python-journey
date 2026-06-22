3132e52094957fc972fba0ee516d607fddf7495b
Ducktty <ckty@duck.com>
2026-06-22 21:20:12 +0800

    Add files via upload

---
+++ analyze.py
@@ -0,0 +1,10 @@
+# 我的第一个数据分析程序
+stocks = [2.5, 4.1, 1.8, 6.3, 3.9, 2.2, 5.7]
+
+average = sum(stocks) / len(stocks)
+highest = max(stocks)
+lowest = min(stocks)
+
+print('平均值:', average)
+print('最高:', highest)
+print('最低:', lowest)
